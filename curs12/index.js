0a1f1a3129fbd7bdde96f1a41ac17661422ff5ee
tanasamarius <tanasamariusiasi@gmail.com>
2017-05-27 14:24:39 +0300

    Add files via upload
    tema de terminat acasa waaa

---
+++ curs12/index.js
@@ -0,0 +1,94 @@
+(function(){
+  										// problema 1
+	// var person = prompt('What is your name?');
+	// if( person != "brian"){
+	// 	document.getElementById('text').innerHTML = "Hello " + person + " nice to meet you";
+	// } else {
+	// 	document.getElementById('text').innerHTML = "Hello " + person + " nice to meet fk off";
+	// }
+
+	// 									problema 2
+	// var sir = prompt('What is the input string?');
+	// document.getElementById('text').innerHTML = 'What is your input string? ' + sir;	
+	// document.getElementById('text2').innerHTML = sir + ' has ' + sir.length + ' char';
+
+
+	// 								 problema 3
+	// var sir = prompt('What is the quote?');
+	// var name = prompt('Who said it?');
+
+	// document.getElementById('text').innerHTML = 'What is the quote? : ' + sir;
+	// document.getElementById('text2').innerHTML =  'who say it: ' + name;
+	// document.getElementById('text3').innerHTML = name + ' ' + 'says: "' + sir +'"';
+
+	// 								problema 4
+	// var noun = prompt('scrie un animal');
+	// var verb = prompt('ce faci cu el');
+	// var adjective = prompt('ce culoare are animalul ?');
+	// var adverb = prompt('cat de repede va miscati');
+
+	// document.getElementById('text1').innerHTML = "enter a noun:" + noun ;
+	// document.getElementById('text2').innerHTML = 'Enter a verb:' + verb;
+	// document.getElementById('text3').innerHTML = 'Enter an adjective:' + adjective;
+	// document.getElementById('text3').innerHTML = 'Enter an adverb:' + adverb;
+	// document.getElementById('text4').innerHTML = 'Do you walk your ' + adjective + ' ' + noun + ' ' + adverb + ' ' + '?' + 'That\'s hilarious!' ;
+
+
+	// 							problema 5
+	// var x = prompt('What is the first number?');
+	// var y = prompt('What is the second number?');
+	// var num1 = parseInt(x);
+	// var num2 = parseInt(y);
+	// document.getElementById('text1').innerHTML = num1 + num2;
+	// document.getElementById('text2').innerHTML = num1 - num2;
+	// document.getElementById('text3').innerHTML = num1 * num2;
+	// document.getElementById('text4').innerHTML = num1 / num2;
+
+	// 						problem 6
+	// var age = prompt('age');
+	// var retire = prompt('retire age?')
+	// var num1 = parseInt(age);
+	// var num2 = parseInt(retire);
+	// document.getElementById('text1').innerHTML = 'you have ' + (retire - age) + ' ' + 'years left until you can retire.';
+	// document.getElementById('text2').innerHTML = 'It\'s 2017, so you can retire in :' + (2017 + (retire - age)); 
+
+
+	// 						problema 7
+	// var length = prompt('What is the length of the room in feet?');
+	// var feet = prompt('What is the width of the room in feet?');
+	// var squareFeet = parseInt(length * feet);
+	// var res = 0.09290304;
+	// document.getElementById('text1').innerHTML = 'You entered dimensions of ' + length + ' ' + 'feet by ' + feet + '.';
+	// document.getElementById('text2').innerHTML = 'The area is:';
+	// document.getElementById('text3').innerHTML =  squareFeet + 'square feet';
+	// document.getElementById('text4').innerHTML = (squareFeet * res) + 'square meters';
+
+			// Math.floor 0.3 , 0.6 = 0
+			// Math.ceil  0.3 , 0.6 = 1
+			// Math.round 0.3 = 0 ; 0.6 = 1
+	// 							problmea 8
+	// var people = prompt('How many people?');
+	// var pizza = prompt('How many pizzas do you have?');
+	// var num1 = 4;
+	// var num2 = Math.floor((num1 * pizza) / people);
+	// var num3 = (num1 * pizza) - (num2 * people);
+	// document.getElementById('text1').innerHTML = 'Each person gets' + ' ' + num2 + ' ' + 'pieces of pizza.';
+	// document.getElementById('text2').innerHTML = 'There are' + ' ' + num3 + ' ' + 'leftover pieces.';
+
+ //                                    problema 9
+	// var length = prompt('length tavanului');
+	// var width = prompt('width tavanului');
+	// var l = parseInt(length);
+	// var w = parseInt(width);
+	// var x = (l * w);
+	// var y = Math.ceil(x / 350);
+	// document.getElementById('qwe').value = x;
+	// document.getElementById('asd').value = y;
+
+
+
+
+
+
+
+})();
