432db1a9830d48cd55211d219b6236e5c1d4b4ae
tanasamarius <tanasamariusiasi@gmail.com>
2017-06-10 14:47:42 +0300

    6.10.2017

---
+++ curs6.10.2017/index.js
@@ -0,0 +1,8 @@
+// (function(){
+// 	document.getElementById('img1').addEventListener('mouseover', function(){
+// 		document.getElementById("group1").style.display = "block";
+// 	});
+// 	document.getElementById('img1').addEventListener('mouseout', function(){
+// 		document.getElementById("group1").style.display = "none";
+// 	});
+// })();
